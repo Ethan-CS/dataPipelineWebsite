58eb0a751456540230008ca6acadfd392c8e5e5e
Ethan Kelly <72452934+ethankelly@users.noreply.github.com>
2022-08-01 09:44:30 +0100

    Update settings.py

--- dataPipelineWebsite/settings.py
+++ dataPipelineWebsite/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vr&5m53k^vzd2=7avg5v4w$5o@g@^uhafeazdon&vmk)6r%%p='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
